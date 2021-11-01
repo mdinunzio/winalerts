dcace9d3531c49d962a92027370fafc1162045bd
Michael DiNunzio <mdinunzio@gmail.com>
2021-10-31 21:12:29 -0400

    added default aguments

--- winalerts.cpp
+++ winalerts.cpp
@@ -12,21 +12,11 @@ namespace py = pybind11;
 #pragma comment (lib, "User32.lib")
 
 
-void alert(){
-	MessageBox(NULL, "Alert", "Alert", MB_OK);
-}
-
 void alert(const std::string& message){
 	MessageBox(NULL, message.c_str(), "Hello", MB_OK);
 }
 
-void alert(const std::string& message, const std::string& title){
-	MessageBox(NULL, message.c_str(), title.c_str(), MB_OK);
-}
-
 PYBIND11_MODULE(winalerts, handle) {
 	handle.doc() = "This is the module docs";
-	handle.def("alert", py::overload_cast<>(&alert));
-	handle.def("alert", py::overload_cast<const std::string&>(&alert));
-	handle.def("alert", py::overload_cast<const std::string&, const std::string&>(&alert));
+	handle.def("alert", &alert, py::arg("message")="Alert");
 }

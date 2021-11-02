2762fb9bf7b8be1d700440d0210d1047cf6f598a
Michael DiNunzio <mdinunzio@gmail.com>
2021-11-01 22:34:35 -0400

    added docstring

--- winalerts.cpp
+++ winalerts.cpp
@@ -1,3 +1,9 @@
+/*
+A module for making alerts and message boxes on Windows.
+
+For more information on Windows alert API see
+https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+*/
 #include <iostream>
 #include <string>
 #include <windows.h>
@@ -14,7 +20,8 @@ namespace py = pybind11;
 
 
 int message_box(const std::string& message, const std::string& title,
-	const std::string& box_type, const std::string& icon_type){
+	const std::string& box_type, const std::string& icon_type,
+	const int default_button){
 	
 	// Box type
 	std::unordered_map<std::string, UINT> box_type_map;
@@ -45,16 +52,41 @@ int message_box(const std::string& message, const std::string& title,
 	};
 	UINT icon_type_int = icon_type_map[icon_type];
 
+	// Default button
+	std::unordered_map<int, UINT> default_button_map;
+	default_button_map = {
+		{1, MB_DEFBUTTON1},
+		{2, MB_DEFBUTTON2},
+		{3, MB_DEFBUTTON3},
+		{4, MB_DEFBUTTON4},
+	};
+	UINT default_button_int = default_button_map[default_button];
 
-	return MessageBox(NULL, message.c_str(), title.c_str(), box_type_int | icon_type_int);
+	return MessageBox(NULL, message.c_str(), title.c_str(),
+		box_type_int | icon_type_int | default_button_int);
 }
 
 PYBIND11_MODULE(winalerts, handle) {
 	handle.doc() = "A module for customizable Windows Alerts.";
 	handle.def(
 		"message_box", &message_box,
+		"Displays a message box to the user.\n\n"
+		"Args:\n",
+		"\tmessage (str): The message that will appear in the box.\n"
+		"\ttitle (str): The text that will appear in the title.\n"
+		"\tbox_type (str): The type of box.\n"
+		"\t\tOne of:  abort_retry_ignore, cancel_continue, help, "
+		"ok, ok_cancel, retry_cancel, yes_no, yes_no_cancel\n"
+		"\t\tDefaults to ok.\n"
+		"\ticon_type (str): The type of icon displayed.\n"
+		"\t\tOne of:  empty, exclamation, warning, information, "
+		"asterisk, question, stop, error, hand\n"
+		"\t\tDefaults to empty.\n"
+		"\tdefault_button (int): The default button, indexed starting at 1.\n"
+		"\tDefaults to 1.",
 		py::arg("message")="Alert",
 		py::arg("title")="Alert",
 		py::arg("box_type")="ok",
-		py::arg("icon_type")="empty");
+		py::arg("icon_type")="empty",
+		py::arg("default_button")=1);
 }

603e6e3635c972767156d8f41d47286baefce9d6
Michael DiNunzio <mdinunzio@gmail.com>
2021-11-01 21:51:27 -0400

    added icon options

--- winalerts.cpp
+++ winalerts.cpp
@@ -14,22 +14,39 @@ namespace py = pybind11;
 
 
 int message_box(const std::string& message, const std::string& title,
-	const std::string& box_type){
+	const std::string& box_type, const std::string& icon_type){
 	
+	// Box type
 	std::unordered_map<std::string, UINT> box_type_map;
 	box_type_map = {
-		{"MB_ABORTRETRYIGNORE", MB_ABORTRETRYIGNORE},
-		{"MB_CANCELTRYCONTINUE", MB_CANCELTRYCONTINUE},
-		{"MB_HELP", MB_HELP},
-		{"MB_OK", MB_OK},
-		{"MB_OKCANCEL", MB_OKCANCEL},
-		{"MB_RETRYCANCEL", MB_RETRYCANCEL},
-		{"MB_YESNO", MB_YESNO},
-		{"MB_YESNOCANCEL", MB_YESNOCANCEL},
+		{"abort_retry_ignore", MB_ABORTRETRYIGNORE},
+		{"cancel_continue", MB_CANCELTRYCONTINUE},
+		{"help", MB_HELP},
+		{"ok", MB_OK},
+		{"ok_cancel", MB_OKCANCEL},
+		{"retry_cancel", MB_RETRYCANCEL},
+		{"yes_no", MB_YESNO},
+		{"yes_no_cancel", MB_YESNOCANCEL},
 	};
 	UINT box_type_int = box_type_map[box_type];
-	
-	return MessageBox(NULL, message.c_str(), title.c_str(), box_type_int);
+
+	// Icon type
+	std::unordered_map<std::string, UINT> icon_type_map;
+	icon_type_map = {
+		{"empty", 0},
+		{"exclamation", MB_ICONEXCLAMATION},
+		{"warning", MB_ICONWARNING},
+		{"information", MB_ICONINFORMATION},
+		{"asterisk", MB_ICONASTERISK},
+		{"question", MB_ICONQUESTION},
+		{"stop", MB_ICONSTOP},
+		{"error", MB_ICONERROR},
+		{"hand", MB_ICONHAND},
+	};
+	UINT icon_type_int = icon_type_map[icon_type];
+
+
+	return MessageBox(NULL, message.c_str(), title.c_str(), box_type_int | icon_type_int);
 }
 
 PYBIND11_MODULE(winalerts, handle) {
@@ -38,5 +55,6 @@ PYBIND11_MODULE(winalerts, handle) {
 		"message_box", &message_box,
 		py::arg("message")="Alert",
 		py::arg("title")="Alert",
-		py::arg("box_type")="MB_OK");
+		py::arg("box_type")="ok",
+		py::arg("icon_type")="empty");
 }
